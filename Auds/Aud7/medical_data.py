5497bb71e6f1c49727f86b28844fcf60b0d5c702
Andrej Mishevski <andrej.mishevski@students.finki.ukim.mk>
2022-05-23 22:27:40 +0200

    aud 7

--- Auds/Aud7/medical_data.py
+++ Auds/Aud7/medical_data.py
@@ -1,6 +1,5 @@
 import csv
-from sklearn.preprocessing import OrdinalEncoder
-from sklearn.naive_bayes import CategoricalNB
+from sklearn.naive_bayes import GaussianNB
 
 
 def read_file(file_name):
@@ -8,7 +7,7 @@ def read_file(file_name):
         csv_reader = csv.reader(doc, delimiter=',')
         dataset = list(csv_reader)[1:]
 
-    dataset_v2  = []
+    dataset_v2 = []
     for row in dataset:
         row_v2 = [int(el) for el in row]
         dataset_v2.append(row_v2)
@@ -28,4 +27,24 @@ if __name__ == '__main__':
     test_y = [row[-1] for row in test_set]
 
     classifier = GaussianNB()
-    print()
+    classifier.fit(train_x, train_y)
+
+    accuracy = 0
+
+    for i in range(len(test_set)):
+        predicted_class = classifier.predict([test_x[i]])[0]
+        true_class = test_y[i]
+        if predicted_class == true_class:
+            accuracy += 1
+
+    accuracy = accuracy / len(test_set)
+
+    print(f'Accuracy: {accuracy}')
+
+    entry = [int(el) for el in input().split(' ')]
+
+    predicted_class = classifier.predict([entry])[0]
+
+    print(predicted_class)
+
+    print(classifier.predict_proba([entry]))
